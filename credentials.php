358f3248b65ff43cb9bc4cddc61db7cd6c383922
Mushtaaq Farah <mushtaaqfar10@gmail.com>
2025-11-01 22:09:54 +0000

    Initial commit of PHP Crude CRUD app

---
+++ credentials.php
@@ -0,0 +1,6 @@
+<?php
+$servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
+$dbname = 'employees';              //which db you're going to use
+$username = 'farah333';
+$password = 'Muslimfalcon.2004';
+?>
